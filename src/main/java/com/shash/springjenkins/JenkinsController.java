e05eceecb77f0cf87508ed4a5ccba4c8f580287e
Shashank S <shashank@softedgesolution.com>
2021-01-02 22:13:51 +0530

    updating rest paths

--- src/main/java/com/shash/springjenkins/JenkinsController.java
+++ src/main/java/com/shash/springjenkins/JenkinsController.java
@@ -2,9 +2,11 @@ package com.shash.springjenkins;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class JenkinsController {
 
     @GetMapping("/hello")
