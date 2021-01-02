8766385efc3b83ff01f6096f066ee6750dc43703
Shashank S <shashank@softedgesolution.com>
2021-01-02 18:45:15 +0530

    first commit

---
+++ src/test/java/com/shash/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shash.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
